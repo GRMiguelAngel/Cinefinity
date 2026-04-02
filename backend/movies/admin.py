cdd047d5b084c40d03284186538f71c77f5d277c
GRMiguelAngel <miguelangel2e2005@gmail.com>
2026-04-02 16:31:56 +0100

    feature: user management and permissions.

--- backend/movies/admin.py
+++ backend/movies/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
 
-from .models import Movie, Genre
-
 # Register your models here.
-
-@admin.register(Movie)
-class MovieAdmin(admin.ModelAdmin):
-    list_display = ['title', 'genre', 'duration']
-
-@admin.register(Genre)
-class GenreAdmin(admin.ModelAdmin):
-    list_display = ['name']
---
+++ frontend/cine/src/components/Body.vue
@@ -0,0 +1,16 @@
+<template>
+    <form action="127.0.0.1/8000/Movies/add" method="POST">
+        <p>Título</p>
+        <input type="text" v-model="title">
+        <p>Género</p>
+        <input type="text" v-model="genre">
+        <p>Duración</p>
+        <input type="time" v-model="duration">
+        <input type="submit" value="Submit">
+    </form>
+</template>
+<script setup>
+</script>
+
+<style scoped>
+</style>
